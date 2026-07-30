020b6583086c9f4839080e13fff249f6c17b8aaf
VasylLisnuy <166537574+VasylLisnuy@users.noreply.github.com>
2026-07-30 16:59:54 +0300

    hw 9

---
+++ HW9.js
@@ -0,0 +1,33 @@
+// function logItems(array) {
+//     for (let i = 0; i < array.length; i += 1) {
+// console.log(`${i + 1} - ${array[i]}`);
+//     }
+// }
+
+// console.log(logItems(['Mango', 'Poly', 'Ajax']));
+
+
+// function calculateEngravingPrice(message, pricePerWord) {
+// const words = message.split(" ")
+// return words.length * pricePerWord
+// }
+
+// console.log(calculateEngravingPrice("Hellow World", 20));
+
+
+function findLongestWord(string) {
+const words = string.split(" ")
+let longestWord = words[0]
+
+for
+
+}
+
+
+
+console.log(findLongestWord("Lorem ipsum dolor sit."));
+console.log(findLongestWord("Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti."));
+
+
+
+
